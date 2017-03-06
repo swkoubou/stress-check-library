61924833162b99345bb3a91730735c537c7ae8fe
TakayaOga <akitsubame010@gmail.com>
2017-03-06 15:56:36 +0900

    修正

--- simple_MA.py
+++ simple_MA.py
@@ -7,20 +7,26 @@ def input_csv(filename):
     data = pd.read_csv(filename)
     return data
 
-#datatimeindex型のデータを単純移動平均する
+#datetimeindex型のデータを単純移動平均する
 def moving_average(data):
     ma=pd.rolling_mean(data, 120)
     return ma
 
 #心拍数の最低値と最高値と平均値を求める
-#def
-    #
+#def check(ma)
 
 #テスト用
 if __name__ == '__main__':
     data=input_csv('heartrate.csv')
     ma=moving_average(data)
 
+    #print(ma.value[120:122])
+    print('合計:',sum(ma.value))
+   
+
+    '''
     data.plot(style='<--')
     ma.plot(style='--', c='r')
     plt.show()
+    '''
+    
